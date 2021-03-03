91c5128fe44bc2d2ed10ea60d628a447b603e982
jinju <gg66477@gmail.com>
2021-03-03 18:05:24 +0900

    level1

---
+++ level1/문자열_가운데글자/solution.py
@@ -0,0 +1,10 @@
+def solution(s):
+    answer = ''
+    if len(s)%2 ==0:
+        answer = s[(len(s)//2)-1]+s[len(s)//2]
+    elif len(s)%2 ==1:
+        answer = s[len(s)//2]
+    return answer
+
+print(solution("abcde"))
+s= "qwer"
