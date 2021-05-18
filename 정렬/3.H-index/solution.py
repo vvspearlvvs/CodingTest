a73ce8d11eda104dc6601f63fb60e11860ea5500
jinju <gg66477@gmail.com>
2021-05-18 19:14:20 +0900

    sort3

---
+++ 정렬/3.H-index/solution.py
@@ -0,0 +1,10 @@
+def solution(citations):
+    citations.sort()
+    l=len(citations)
+    for i in range(l):
+        if citations[i]>= l-i:
+            return l-i
+    return 0
+
+print(solution([3, 0, 6, 1, 5]))
+#print(count([3, 0, 6, 1, 5],1))
