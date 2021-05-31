0f70c18b621031dc216ad25be3ba7e6c92bd4ca3
jinju <gg66477@gmail.com>
2021-05-31 23:55:30 +0900

    programmers

---
+++ DFSBFS/1.타켓넘버/solution.py
@@ -0,0 +1,12 @@
+def solution(numbers, target):
+    super = [0]
+    for i in numbers:
+        sub=[]
+        for j in super:
+            sub.append(j+i)
+            sub.append(j-i)
+        super=sub
+    return super.count(target)
+
+input = [1, 1, 1, 1, 1]
+print(solution(input,3))
