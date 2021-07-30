56708daea6667d18515a3965c982826c9fa090a6
jinju <gg66477@gmail.com>
2021-07-31 00:17:00 +0900

    programmers

--- 2.프로그래머스 고득점Kit/DFSBFS/4.여행경로/stack.py
+++ 2.프로그래머스 고득점Kit/DFSBFS/4.여행경로/stack.py
@@ -2,7 +2,7 @@ from collections import defaultdict
 
 def solution(tickets):
     answer=[]
-    routes=defaultdict(list)
+    routes=defaultdict(list) #{시작지점:[도착지점]}
 
     for ticket in tickets:
         start=ticket[0]
@@ -10,7 +10,7 @@ def solution(tickets):
         routes[start].append(end)
     print(routes) #list: {'ICN': ['JFK'], 'HND': ['IAD'], 'JFK': ['HND']}
 
-    #가능한경로가2개일경우, 알파벳순서가 앞서는 순서가 되기 위해
+    #도착지점이 2개이상일 경우, 알파벳순서가 앞서는 순서가 되기 위해
     for key in routes.keys():
         routes[key].sort(reverse=True)
     print(routes) #list: {'ICN': ['JFK'], 'HND': ['IAD'], 'JFK': ['HND']}
@@ -20,11 +20,13 @@ def solution(tickets):
         print("stack",stack)
         tmp=stack[-1]
         print("stack top",tmp)
-        if not routes[tmp]:
-            answer.append(stack.pop())
-        else:
+
+        if not routes[tmp]: #끝까지 탐색해서 도착지접이 없을 경우
+            answer.append(stack.pop()) #도착한 순서대로 answer에 넣음
+        else: #도착지점->시작지점 이어지면, 다음도착지점 stack에 넣음
             stack.append(routes[tmp].pop())
-    answer.reverse()
+    print(answer)
+    answer.reverse() #탐색하는 순서는 도착한순서랑 반대
     return answer
 
 test1=[["ICN", "JFK"], ["HND", "IAD"], ["JFK", "HND"]]
